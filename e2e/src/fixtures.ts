3aab1e26f03505dadaefc37c3aaea3432456e3ed
Matt Raible <matt.raible@crowdstrike.com>
2026-05-07 13:13:58 -0600

    Trim fixtures to only what tests actually use

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,55 +1,13 @@
 import { test as baseTest } from '@playwright/test';
-import {
-  AppCatalogPage,
-  WorkflowsPage,
-  DetectionExtensionPage,
-  HostExtensionPage,
-  CaseExtensionPage,
-  WorkbenchExtensionPage,
-  AutomatedLeadsExtensionPage,
-  WorkflowExecutionExtensionPage,
-  config,
-} from '@crowdstrike/foundry-playwright';
+import { DetectionExtensionPage } from '@crowdstrike/foundry-playwright';
 
 type FoundryFixtures = {
-  appCatalogPage: AppCatalogPage;
-  workflowsPage: WorkflowsPage;
   detectionExtensionPage: DetectionExtensionPage;
-  hostExtensionPage: HostExtensionPage;
-  caseExtensionPage: CaseExtensionPage;
-  workbenchExtensionPage: WorkbenchExtensionPage;
-  automatedLeadsExtensionPage: AutomatedLeadsExtensionPage;
-  workflowExecutionExtensionPage: WorkflowExecutionExtensionPage;
-  appName: string;
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  appCatalogPage: async ({ page }, use) => {
-    await use(new AppCatalogPage(page));
-  },
-  workflowsPage: async ({ page }, use) => {
-    await use(new WorkflowsPage(page));
-  },
   detectionExtensionPage: async ({ page }, use) => {
     await use(new DetectionExtensionPage(page));
-  },
-  hostExtensionPage: async ({ page }, use) => {
-    await use(new HostExtensionPage(page));
-  },
-  caseExtensionPage: async ({ page }, use) => {
-    await use(new CaseExtensionPage(page));
-  },
-  workbenchExtensionPage: async ({ page }, use) => {
-    await use(new WorkbenchExtensionPage(page));
-  },
-  automatedLeadsExtensionPage: async ({ page }, use) => {
-    await use(new AutomatedLeadsExtensionPage(page));
-  },
-  workflowExecutionExtensionPage: async ({ page }, use) => {
-    await use(new WorkflowExecutionExtensionPage(page));
-  },
-  appName: async ({}, use) => {
-    await use(config.appName);
   },
 });
 
